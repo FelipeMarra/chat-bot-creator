8770504de0db7c1ebfaed72d3c9d325a7c24162e
Felipe Marra <felipeferreiramarra@aluno.ufsj.edu.br>
2022-04-30 16:02:16 -0300

    test bd again

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -1,3 +1,4 @@
+import sys
 from asyncio import run, set_event_loop_policy, WindowsSelectorEventLoopPolicy
 
 from connection import engine
@@ -6,12 +7,16 @@ from base import Base
 async def create_db():
     async with engine.begin() as connection:
         print("2")
+        sys.stdout.flush()
         await connection.run_sync(Base.metadata.drop_all)
         print("3")
+        sys.stdout.flush()
         await connection.run_sync(Base.metadata.create_all)
 
 if __name__ == "__main__":
     #set_event_loop_policy(WindowsSelectorEventLoopPolicy())
     print("1")
+    sys.stdout.flush()
     run(create_db())
     print("4")
+    sys.stdout.flush()
