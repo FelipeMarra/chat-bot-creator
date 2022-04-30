536edfe70e5a1d75464988e480330ac9957ad726
Felipe Marra <felipeferreiramarra@aluno.ufsj.edu.br>
2022-04-30 15:58:28 -0300

    test bd again

--- app/db/connection.py
+++ app/db/connection.py
@@ -4,7 +4,8 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 
-DB_URL = "postgresql+asyncpg://aoyjyqpilyfguy:bb7b2a0935f79cda7380a256cfae7668b44f89cc88a6e112b77438aeeba9c261@ec2-34-194-73-236.compute-1.amazonaws.com:5432/dpie2r7lfsq85"
+#DB_URL = "postgresql+asyncpg://aoyjyqpilyfguy:bb7b2a0935f79cda7380a256cfae7668b44f89cc88a6e112b77438aeeba9c261@ec2-34-194-73-236.compute-1.amazonaws.com:5432/dpie2r7lfsq85"
+DB_URL = getenv("DB_URL")
 
 engine = create_async_engine(DB_URL)
 async_session = sessionmaker(engine, class_=AsyncSession)
--- app/db/init_db.py
+++ app/db/init_db.py
@@ -5,9 +5,13 @@ from base import Base
 
 async def create_db():
     async with engine.begin() as connection:
+        print("2")
         await connection.run_sync(Base.metadata.drop_all)
+        print("3")
         await connection.run_sync(Base.metadata.create_all)
 
 if __name__ == "__main__":
     #set_event_loop_policy(WindowsSelectorEventLoopPolicy())
+    print("1")
     run(create_db())
+    print("4")
