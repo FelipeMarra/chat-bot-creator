5ab52d56537fc8cd03fd70c7e229ea5a15cedd63
Felipe Marra <felipeferreiramarra@aluno.ufsj.edu.br>
2022-04-30 22:11:55 -0300

    .

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
+import socket
+import sys
+
 from fastapi import FastAPI
 from app.views.user_view import user_router
 
 #start app
 app = FastAPI()
 
+hostname = socket.gethostname()
+
+version = f"{sys.version_info.major}.{sys.version_info.minor}"
+
+
 #add routs
+@app.get("/")
+async def read_root():
+    return {
+        "name": "chat-bot-creator-back",
+        "host": hostname,
+        "version": f"Hello world! From FastAPI running on Uvicorn. Using Python {version}"
+    }
+
 app.include_router(user_router)
+
