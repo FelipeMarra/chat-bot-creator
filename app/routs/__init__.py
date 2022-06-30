042d5cb66c86adc21f4f662e569a9e16b05c62b9
Felipe Marra <felipeferreiramarra@aluno.ufsj.edu.br>
2022-06-30 16:28:14 -0300

    .import router

--- app/routs/__init__.py
+++ app/routs/__init__.py
@@ -4,3 +4,4 @@ from .login_rout import login_router
 from .chat_rout import chatbot_router
 from .base_state_rout import base_state_router
 from .single_choice_rout import single_choice_router
+from .multiple_choice_rout import multiple_choice_router
